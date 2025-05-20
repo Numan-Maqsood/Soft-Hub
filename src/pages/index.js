ba669d18a0d62b4c02ad289cc98a6c465f4c0a79
Numan Maqsood <numanmaqsood01@gmail.com>
2025-05-20 16:50:24 +0500

    create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,44 @@ export default function Index({ HomeData }) {
       <Header />
 
       {/* Home Page */}
-      <section className="relative h-screen flex items-center">
+      <section className="BannerSection relative h-[100vh] iPad:h-[90vh] lg:h-[70vh] xl:h-screen flex items-center pb-9">
         <img className="w-full h-full absolute left-0 right-0 top-0 bottom-0 object-cover" src="BannerBg.png" alt="Banner Clouds" />
-        <div className="BannerContent max-w-[960px] mx-auto text-center relative">
-          <h1><span className="text-themeSky">Delivering Digital</span> Transformation & E-learning </h1>
-          <p className="text-[18px] font-semibold text-themeBlue mt-[20px] mb-[40px]">Founded in 2021, SoftHub is an innovative digital agency in Dubai, UAE. It excels in cutting-edge solutions and prioritizes client needs, leveraging creativity and tech expertise for diverse clients.</p>
-          <Link className="btn" href="#.">Schedule a Demo</Link>
+        <div className="container relative z-[9]">
+          <div className="BannerContent h-max-w-[960px] md:max-w-[860px] lg:max-w-[960px] mx-auto text-center relative">
+            <h1><span className="text-[#007DF5]">Delivering Digital</span> Transformation & E-learning </h1>
+            <p className="text-lg font-semibold text-themeBlue mt-[20px] mb-[30px] lg:mb-[40px] xl:mb-[40px]">Founded in 2021, SoftHub is an innovative digital agency in Dubai, UAE. It excels in cutting-edge solutions and prioritizes client needs, leveraging creativity and tech expertise for diverse clients.</p>
+            <Link className="btn" href="#.">Schedule a Demo</Link>
+          </div>
+        </div>
+        <div className="container absolute left-0 right-0 bottom-20">
+          <div className="flex justify-between items-center">
+            <div className="Tags LeftTags">
+              <ul>
+                <li>
+                  <Link href="#.">Development</Link>
+                </li>
+                <li>
+                  <Link href="#.">Web</Link>
+                </li>
+                <li>
+                  <Link href="#.">Mobile</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="Tags RightTags">
+              <ul>
+                <li>
+                  <Link href="#.">Data</Link>
+                </li>
+                <li>
+                  <Link href="#.">UI/UX</Link>
+                </li>
+                <li>
+                  <Link href="#.">ERP</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
     </>
